91d3d538e22a8c4799209c4ccf5ab12277618a23
Mohammed <88824957+m7medVision@users.noreply.github.com>
2025-04-05 09:00:56 +0000

    chore: configure MDsvex to handle .md files in SvelteKit

--- new_app/svelte.config.js
+++ new_app/svelte.config.js
@@ -3,9 +3,11 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [vitePreprocess(), mdsvex({
+		extensions: ['.md', '.svx']
+	})],
 	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx']
+	extensions: ['.svelte', '.svx', '.md']
 };
 
 export default config;
